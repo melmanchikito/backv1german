bcff861276e5b4ed7c4eeaa17f7361f9910d4757
German Machado <germanbmachado@gmail.com>
2026-02-04 14:27:53 -0500

    fix cors api3

--- Controllers/ReservaController.php
+++ Controllers/ReservaController.php
@@ -26,7 +26,7 @@ class ReservaController
     private function jsonHeaders()
     {
         header('Content-Type: application/json');
-        header("Access-Control-Allow-Origin: *");
+
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
         header("Access-Control-Allow-Headers: Content-Type");
     }
